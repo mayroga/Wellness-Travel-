f62fb97ca80843b879cdca103a9bd4441dc26c03
mayroga <132472344+mayroga@users.noreply.github.com>
2026-08-31 19:13:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -312,8 +312,8 @@ async def crear_sesion_checkout_elite(request: Request):
             }],
             mode='payment' if "unico" in price_id else 'subscription',
             # BLINDAJE INMUTABLE CON TU URL OFICIAL EN RENDER:
-            success_url=f"https://onrender.com{folio_servicio}",
-            cancel_url="https://onrender.com",
+            success_url=f"https://wellness-travel.onrender.com{folio_servicio}",
+            cancel_url="https://wellness-travel.onrender.com",
             client_reference_id=folio_servicio
         )
         return JSONResponse(status_code=200, content={"id": session.id, "url": session.url})
